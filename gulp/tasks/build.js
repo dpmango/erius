0bd7c8476619d716bed826af1fc6c2fbe130f460
Sergey Khmelevskoy <xs290@me.com>
2019-04-01 22:47:46 +0300

    moved current build to gulp

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,31 +1,28 @@
-var gulp        = require('gulp');
-var runSequence = require('run-sequence');
-var config      = require('../config');
+var gulp = require("gulp");
+var runSequence = require("run-sequence");
+var config = require("../config");
 
 // set build evnironment first and perform build() function
-gulp.task('build:production', function(callback) {
-  config.setEnv('production');
+gulp.task("build:production", function(callback) {
+  config.setEnv("production");
   config.logEnv();
   build(callback);
 });
 
-gulp.task('build:development', function(callback) {
-  config.setEnv('development');
+gulp.task("build:development", function(callback) {
+  config.setEnv("development");
   config.logEnv();
   build(callback);
 });
 
 function build(callback) {
   runSequence(
-    'clean:dist',
-    'sass',
-    'pug',
-    'javascript',
-    'images',
-    'sprite:svg',
-    'sprite:png',
-    'copy',
-    'list-pages',
+    "clean:dist",
+    "sass",
+    "pug",
+    "javascript",
+    "images",
+    "copy",
     callback
   );
 }
